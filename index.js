4bb8b713a37f28cbdbee9aa6c7479d97f4d7577c
nope <nope@nope.nope>
2016-11-24 15:16:16 +0100

    fixed port

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var restify = require('restify');
+var port = process.argv[2];
 
 function respond(req, res, next) {
   res.send('hello ' + req.params.name);
@@ -9,6 +10,6 @@ var server = restify.createServer();
 server.get('/hello/:name', respond);
 server.head('/hello/:name', respond);
 
-server.listen(8080, function() {
+server.listen(port, function() {
   console.log('%s listening at %s', server.name, server.url);
 });
